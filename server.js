36697db0ddc6cb3b12258ec97ec60949d8075dcb
Matthew <Matirix@live.com>
2023-09-26 11:24:57 -0700

    added port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http')
 const util = require('./modules/utils')
+PORT = `https://outrageous-battledress-seal.cyclic.cloud` || 8080
 
 app.get('/', function (req, res) {
     const name = req.query.name;
@@ -11,7 +12,7 @@ app.get('/', function (req, res) {
     res.send(`<b> ${message} </b>`)
 })
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log('Server running on port 3001.')
 })
 console.log('Server running on port 8080.')
